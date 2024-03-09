0fe641e9336556c813cb3638355c6b16a1013eb0
jniyonkuru <niyonkurujacques@gmail.com>
2024-03-09 09:43:30 +0200

    work on liking a blog post

--- src/controllers/blogcontroller.js
+++ src/controllers/blogcontroller.js
@@ -2,7 +2,8 @@ import Blog from "../models/blogmodel.js"
 import ValidateBlog from "../utils/validateblog.js";
 import _ from "lodash"
 import jwt from 'jsonwebtoken';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
+
 dotenv.config()
 
 export default class BlogController{
@@ -13,7 +14,10 @@ export default class BlogController{
             message:error.details[0].message
         })
       try {
-        const blog= new Blog(_.pick(req.body,['blogTitle','image','author','blogBody']));
+        const blog= new Blog(_.pick(req.body,['blogTitle','image','blogBody']));
+        const decoded = jwt.verify(req.header('x-auth-token',process.env.JWT_SECRET));
+        blog.author=decoded.userId;
+         
       await blog.save()
       return res.status(204).send()
       } catch (error) {
@@ -88,63 +92,27 @@ static async likeBlog(req,res){
 try {
     const decoded=jwt.verify(req.header('x-auth-token'),process.env.JWT_SECRET);
     const userId=decoded.userId;
-    const likedBlog= await Blog.findOne({_id:req.params.id,likedBy:{$in:userId}})
-    if(likedBlog) return res.status(400).json({
-        status:"fail",
-        message:"You already liked the blog"
-    })
-  const blog =await Blog.findByIdAndUpdate(req.params.id,{$push:{likedBy:userId},$inc:{likes:1},$pull:{dislikedBy:userId}},{new:true})
+    const likedBlog= await Blog.findOne({_id:req.params.id,likedBy:{$in:userId}});
+
+   if(likedBlog){
+    const blog= await Blog.findByIdAndUpdate(req.params.id,{$pull:{likedBy:userId},$inc:{likes:-1}},{new:true});
+     return res.status(200).json({
+        status:"success",
+        message:"disliked a post"
+     })
+   }else{
+  const blog =await Blog.findByIdAndUpdate(req.params.id,{$push:{likedBy:userId},$inc:{likes:1}},{new:true})
   res.status(200).json({
     status:'success',
-    message:{
-        message:"you liked a post",
-        data:blog.likedBy
-    },
-    
+  message:"you liked a post",
   })  
-
+   }
 } catch (error) {
     return res.status(500).json({
         status:'error',
         message:error.message
-    })
-    
+    })  
 }
-}
-static async dislikeBlog(req,res){
-    
-    try {
-
-   const decoded=jwt.verify(req.header('x-auth-token'),process.env.JWT_SECRET);
-    const userId=decoded.userId;
-    const likedBLog= await Blog.findOne({_id:req.params.id,likedBy:{$in:userId}});
-    // const dislikedBLog= await Blog.findOne({_id:req.params.id,dislikedBy:{$in:userId}});
-    if(!likedBLog)return res.status(400).json({
-        status:'fail',
-        message:'You did not like the post before'
-    }) 
-    // if(dislikedBLog) return res.status(400).json({
-    //     status:"fail",
-    //     message:" you already disliked the post"
-    // })
-    const blog=await Blog.findByIdAndUpdate(req.params.id,{$push:{dislikedBy:userId},$pull:{likedBy:userId},$inc:{likes:-1}},{new:true})
-
-      return res.status(200).json({
-        status:"success",
-        message:"you disliked a post"
-
-      })
-        
-    } catch (error) {
-
-        return res.status(500).json({
-            status:'error',
-            message:error.message
-        })
-        
-    }
-
-
 }
 
 }
